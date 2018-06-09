d926f4aaa69723645a82cd44e45e588cbe668ff9
Dan Poxton <danbobs27@gmail.com>
2018-06-09 15:36:43 +0100

    Formatting, missed z param.

--- CSharpSeven/ValueTuples/DeconstructPoint.cs
+++ CSharpSeven/ValueTuples/DeconstructPoint.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSharpSeven.ValueTuples
+﻿namespace CSharpSeven
 {
     public class DeconstructPoint
     {
 
         public void Deconstruct()
         {
-            var point = new Point(1, 2);
-
+            var point = new Point(1, 2, 3);
             var (x, y, z) = point;
             (x,y) = point;
         }
@@ -25,11 +18,10 @@ namespace CSharpSeven.ValueTuples
         public int Y { get; }
         public int Z { get; }
 
-        public Point(int x, int y) { X = x; Y = y; }
+        public Point(int x, int y, int z) { X = x; Y = y; Z = z}
         // Out params!? WHHHHYYYYYY? Why not return a tuple here? The stupid, IT BURNS!!!
         public void Deconstruct(out int x, out int y, out int z) { x = X; y = Y; z = Z; }
         // apparently for this reason, to allow overloads to allow different number of items in tuple
         public void Deconstruct(out int x, out int y) { x = X; y = Y; }
-
     }
 }
