4be03e8753412b0b2855ae4ae0620f1699261579
Dan Poxton <danbobs27@gmail.com>
2018-06-09 23:05:09 +0100

    Out variables example

---
+++ CSharpSeven/OutVariables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeven
+{
+    public class OutVariables
+    {
+
+        public void OldSchool()
+        {
+            var input = "42";
+            
+            // Clunkety clunk clunk
+            int i;
+            var success = int.TryParse(input, out i);
+        }
+
+        public void NewStyle()
+        {
+            var input = "42";
+
+            // Slightly less clunk
+            var success = int.TryParse(input, out int i);
+        }
+
+    }
+}
