44f61d221d11aa826a66aa4437bf0480e2de51ed
Dan Poxton <danbobs27@gmail.com>
2018-06-09 15:43:06 +0100

    Missed semi-colon.

--- CSharpSeven/ValueTuples/DeconstructPoint.cs
+++ CSharpSeven/ValueTuples/DeconstructPoint.cs
@@ -18,7 +18,7 @@
         public int Y { get; }
         public int Z { get; }
 
-        public Point(int x, int y, int z) { X = x; Y = y; Z = z}
+        public Point(int x, int y, int z) { X = x; Y = y; Z = z; }
         // Out params!? WHHHHYYYYYY? Why not return a tuple here? The stupid, IT BURNS!!!
         public void Deconstruct(out int x, out int y, out int z) { x = X; y = Y; z = Z; }
         // apparently for this reason, to allow overloads to allow different number of items in tuple
