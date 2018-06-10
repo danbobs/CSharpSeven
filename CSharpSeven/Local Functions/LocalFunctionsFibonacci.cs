8540ed670d59c84bef9af2e26342a5f3f88b3454
Dan Poxton <danbobs27@gmail.com>
2018-06-10 13:06:45 +0100

    Local functions examples.

---
+++ CSharpSeven/Local Functions/LocalFunctionsFibonacci.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeven.Local_Functions
+{
+    public class LocalFunctionsFibonacci
+    {
+
+        // courtesy of Mads Torgeson https://blogs.msdn.microsoft.com/dotnet/2017/03/09/new-features-in-c-7-0/
+        // C#7 Local Function
+        public int Fibonacci(int x)
+        {
+            if (x < 0) throw new ArgumentException("Less negativity please!", nameof(x));
+            return Fib(x).current;
+
+            (int current, int previous) Fib(int i)
+            {
+                if (i == 0) return (1, 0);
+                var (p, pp) = Fib(i - 1);
+                return (p + pp, p);
+            }
+        }
+
+        // alternative using internal helper function
+        public int Fibonacci2(int x)
+        {
+            if (x < 0) throw new ArgumentException("Less negativity please!", nameof(x));
+            return FibInternal (x).current;       
+        }
+
+        private (int current, int previous) FibInternal(int i)
+        {
+            if (i == 0) return (1, 0);
+            var (p, pp) = FibInternal(i - 1);
+            return (p + pp, p);
+        }
+
+        // alternative using anonymous lambda function 
+        //public int Fibonacci(int x)
+        //{
+
+        //    Func<int, (int, int)> fib = (int i) =>
+        //    {
+        //        if (i == 0) return (1, 0);
+        //        var (p, pp) = fib(i - 1); // nope, can recurse with an anonymous function
+        //        return (p + pp, p);
+        //    };
+
+        //    if (x < 0) throw new ArgumentException("Less negativity please!", nameof(x));
+        //    return fib(x).Item1;
+        //}
+
+    }
+}
---
+++ CSharpSeven/Local Functions/LocalFunctionsFilter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSeven
+{
+    public class LocalFunctionsFilter
+    {
+
+        // Courtesy of Mads Torgeson https://blogs.msdn.microsoft.com/dotnet/2017/03/09/new-features-in-c-7-0/
+        // C#7 Local Function
+        public IEnumerable<T> Filter<T>(IEnumerable<T> source, Func<T, bool> filter)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (filter == null) throw new ArgumentNullException(nameof(filter));
+            return Iterator();
+
+            IEnumerable<T> Iterator()
+            {
+                foreach (var element in source)
+                {
+                    if (filter(element)) { yield return element; }
+                }
+            }
+        }
+
+        // alternative using private helper function
+        public IEnumerable<T> Filter2<T>(IEnumerable<T> source, Func<T, bool> filter)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (filter == null) throw new ArgumentNullException(nameof(filter));
+            return FilterIterator(source, filter);
+        }
+
+        private IEnumerable<T> FilterIterator<T>(IEnumerable<T> source, Func<T, bool> filter)
+        {
+            foreach (var element in source)
+            {
+                if (filter(element)) { yield return element; }
+            }
+        }
+
+        // cons: 
+        // - less well encapuslated
+        // - replicated template syntax & args again on internal function
+        // - users of class might use internal func unwittingly and skip the param checking
+
+
+        // alternative using anonymous lambda function
+        public IEnumerable<T> Filter3<T>(IEnumerable<T> source, Func<T, bool> filter)
+        {
+            // Nope! Yield not supported in anoynmous 
+            //Func<IEnumerable<T>> iterator = () =>
+            //{
+            //    foreach (var element in source)
+            //    {
+            //        if (filter(element)) { yield return element; }
+            //    }
+            //}
+
+            Func<IEnumerable<T>> iterator = () => source.Where(e => filter(e)); // doesn't yield
+
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (filter == null) throw new ArgumentNullException(nameof(filter));
+            return iterator();
+
+        }
+
+
+
+    }
+}
